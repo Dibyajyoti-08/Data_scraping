62f7b4d34458af1f2f315791aafc4ab9c95ddd5f
djena <eng.djena075@gmail.com>
2024-09-16 22:07:31 +0530

    Processing tweets and vectorizing

--- data_scraping.py
+++ data_scraping.py
@@ -1,4 +1,6 @@
 import tweepy
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 consumer_key =  ''
 consumer_secret = ''
@@ -6,8 +8,8 @@ access_token = ''
 access_token_secret = ''
 
 # Authenticate with
-auth = tweepy.0Auth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api tweepy.API(auth)
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
 
 
 # Scrape tweets
@@ -19,3 +21,28 @@ def fetch_tweets(keyword, count=100):
 tweets = fetch_tweets("Python", 200)
 for tweet in tweets[:5]:
     print(tweet)
+
+
+# Process tweet data
+def clean_tweet(tweet):
+    # Remove URLs, mentions and hashtag
+    tweet = re.sub(r'http\S+|@\S+|#\S', '', tweet)
+
+    # Remove special characters and numbers
+    tweet = re.sub(r'[^A-Za-z\s]', '', tweet)
+
+    # Convert to lowercase
+    tweet = tweet.lower()
+    
+    return tweet
+
+# Apply the processing
+cleaned_tweets = [clean_tweet(tweet[0]) for tweet in tweets]
+
+# Tokenization and Vectorization
+'''
+Convertion of cleaned tweets into numerical 
+representations using TF-IDF or Tokenizer.
+'''
+vectorizer = TfidfVectorizer(max_features=5000)
+X = vectorizer.fit_transform(cleaned_tweets).toarray()
