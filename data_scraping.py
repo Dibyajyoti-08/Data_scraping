7a886c04e3372d28b6131133b95dc35fc3b3ee72
djena <eng.djena075@gmail.com>
2024-09-21 10:27:10 +0530

    Error handling while fetching tweets

--- data_scraping.py
+++ data_scraping.py
@@ -10,16 +10,19 @@ consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-# Authenticate with
+# Authenticate with Twitter APi
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
 # Scrape tweets
 def fetch_tweets(keyword, count=100):
-    tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en", tweet_mode='extended').items(count)
-    tweet_list = [[tweet.full_text, tweet.created_at] for tweet in tweets]
-    return tweet_list
+    try:
+        tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en", tweet_mode='extended').items(count)
+        tweet_list = [[tweet.full_text, tweet.created_at] for tweet in tweets]
+        return tweet_list
+    except tweepy.TweepError as e:
+        print(f"Error fetching tweets: {e}")
 
 tweets = fetch_tweets("Python", 200)
 for tweet in tweets[:5]:
